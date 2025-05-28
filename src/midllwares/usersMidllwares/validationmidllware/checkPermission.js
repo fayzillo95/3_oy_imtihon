e89bc03f220e8293763b15d1f44e0cc474c967c8
Fayzillo <ovovovlululutvata@gmail.com>
2025-05-28 18:10:48 +0500

    a

--- src/midllwares/usersMidllwares/validationmidllware/checkPermission.js
+++ src/midllwares/usersMidllwares/validationmidllware/checkPermission.js
@@ -12,10 +12,16 @@ export default async function checkPermission(req, res, next) {
 
         let url = req.url.split("/")
         let model = url[url.indexOf("api") + 1]
-        let branch_id = req.headers['branch_id']
         let method = req.method
-
-        if(!branch_id) throw new CustomError("branch_id headersdan berilmagan !",400)
+        
+        let branch_id;
+        if(["POST", "PUT"]){
+            branch_id = req.headers['branch_id']
+            if(!branch_id) throw new CustomError("branch_id headersdan berilmagan !",400)
+        }else{
+            branch_id = req.body.branch_id
+            if(!branch_id) throw new CustomError("branch_id bodydan berilmagan !",400)
+        }
 
         const permission = await permissionModel.findOne({
             user_id:user._id,
