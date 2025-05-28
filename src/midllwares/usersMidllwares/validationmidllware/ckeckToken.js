0ffdb64cdcfc8ec42b4ed35fb81ff76d3354a33e
Fayzillo <ovovovlululutvata@gmail.com>
2025-05-28 16:49:26 +0500

    1

--- src/midllwares/usersMidllwares/validationmidllware/ckeckToken.js
+++ src/midllwares/usersMidllwares/validationmidllware/ckeckToken.js
@@ -1,9 +1,12 @@
-import { AuthorizationError } from "../../../utils/errors/Vlaidatio.error.js"
+import { AuthorizationError, CustomError } from "../../../utils/errors/Vlaidatio.error.js"
 import { veryToken } from "../../../utils/token/tokengeneretor.js"
 
 export const checkToken = (req, res, next) => {
     try {
         const auth = req.headers.authorization
+        if(!auth){
+            throw new CustomError("Token topilmadi !",401)
+        }
         if(!auth.startsWith("Bearer ")) throw new AuthorizationError("Invalid type token !")
         const token = auth.split(" ")[1]
         const {id} = veryToken(token)
